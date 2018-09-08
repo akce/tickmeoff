8a19485b2dc8923e375187dc8f5482bd99463326
Akce <>
2018-09-08 15:19:49 +1000

    Drop the agent string from url requests.

--- tickmeoff/tickmeoff.py
+++ tickmeoff/tickmeoff.py
@@ -10,10 +10,7 @@ from . import imdb
 from . import movie
 
 def openurl(url):
-    ## NOTE: the agent value usually affects the content format returned by the server!
-    #agent = 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
-    agent = 'Lynx/2.8.8rel.2 libwww-FM/2.14 SSL-MM/1.4.1'
-    req = ur.Request(url, headers={'User-Agent': agent})
+    req = ur.Request(url)
     return ur.urlopen(req)
 
 def fileopen(filename):
